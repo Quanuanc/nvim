efac135761c7adaa3804a3b60c032d8ff6bd060a
Quanuanc <quanuanc@outlook.com>
2022-10-28 22:14:31 +0800

    Update

--- lua/user/setups.lua
+++ lua/user/setups.lua
@@ -1,1 +1,14 @@
-require('im_select').setup()
+local plugin_list = {
+  im_select = {},
+}
+
+for p, opt in pairs(plugin_list) do
+  local status_ok, plugin = pcall(require, p)
+  if not status_ok then
+    goto continue
+  end
+
+  plugin.setup(opt)
+
+  ::continue::
+end
