2e609adb3e152946312a85bea2fa6cae10ad469f
Quanuanc <quanuanc@outlook.com>
2022-11-05 23:10:48 +0800

    Add lsp and null-ls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -59,6 +59,13 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "RRethy/vim-illuminate"
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
