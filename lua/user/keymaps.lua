4ecc5fd256be8ff3ba49d4336c94de9b4e3c0ca6
Quanuanc <quanuanc@outlook.com>
2022-11-06 10:58:20 +0800

    Add telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -34,6 +34,10 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<cr>", opts)
+keymap("n", "<leader>fw", ":Telescope live_grep<cr>", opts)
+
 -- System clipboard
 keymap("n", "<leader>y", "\"+y", opts)
 keymap("v", "<leader>y", "\"+y", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,8 @@ return packer.startup(function(use)
   use "jose-elias-alvarez/null-ls.nvim"
   use "RRethy/vim-illuminate"
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
 
   if PACKER_BOOTSTRAP then
   	require("packer").sync()
