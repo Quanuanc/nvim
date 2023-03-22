fdffc6af7ac26ecb873148af73515ee5dd4841e4
全国成 <quanuanc@outlook.com>
2023-03-22 14:02:45 +0800

    Update

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -8,7 +8,6 @@ local options = {
 	mouse = "", -- allow the mouse to be used in neovim
 	pumheight = 10, -- pop up menu height
 	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-	showtabline = 2, -- always show tabs
 	smartcase = true, -- smart case
 	smartindent = true, -- make indenting smarter again
 	splitbelow = true, -- force all horizontal splits to go below current window
@@ -27,14 +26,12 @@ local options = {
 	relativenumber = true, -- set relative numbered lines
 	numberwidth = 2, -- set number column width to 2 {default 4}
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-	wrap = true, -- display lines as one long line
+	wrap = false, -- display lines as one long line
 	linebreak = true, -- companion to wrap, don't split words
 	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
 	sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
 	clipboard = "unnamed",
 }
-
-vim.opt.shortmess:append("c")
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
