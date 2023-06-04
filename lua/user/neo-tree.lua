34a419445851470a1ab995054d66b362c1f66109
Quanuanc <quanuanc@outlook.com>
2023-06-04 17:19:06 +0800

    Remove neo-tree git status icon

--- lua/user/neo-tree.lua
+++ lua/user/neo-tree.lua
@@ -4,7 +4,23 @@ if not status_ok then
 end
 
 neo_tree.setup({
+	enable_git_status = true,
 	enable_diagnostics = false,
+	default_component_configs = {
+		git_status = {
+			symbols = {
+				added = "",
+				modified = "",
+				deleted = "",
+				renamed = "",
+				untracked = "",
+				ignored = "",
+				unstaged = "",
+				staged = "",
+				conflict = "",
+			},
+		},
+	},
 	window = {
 		mapping_options = {
 			noremap = true,
