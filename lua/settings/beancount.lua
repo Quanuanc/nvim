e92a71f99b31378bb80b3917f3da1fa6faf2bfca
Quanuanc <quanuanc@outlook.com>
2023-12-09 00:07:06 +0800

    remote some lsp

--- lua/settings/beancount.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-local journal_file = vim.loop.cwd() .. "/" .. "main.beancount"
-
-M.init_options = {
-  journal_file = journal_file,
-}
-
-return M
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,12 +2,6 @@ local M = {}
 
 M.servers = {
   "sumneko_lua",
-  "pyright",
-  "rust_analyzer",
-  "beancount",
-  "tsserver",
-  "jsonls",
-  "clangd",
 }
 
 return M
