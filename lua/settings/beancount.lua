e15b233d6165f7dd099dcefef14901cdd78e298b
Quanuanc <quanuanc@outlook.com>
2023-12-30 09:12:43 +0800

    update beancount lsp setting

--- lua/settings/beancount.lua
+++ lua/settings/beancount.lua
@@ -2,6 +2,11 @@ local M = {}
 
 local journal_file = vim.loop.cwd() .. "/" .. "main.beancount"
 
+M.cmd = {
+  "beancount-language-server",
+  "--stdio",
+  "stdio",
+}
 M.init_options = {
   journal_file = journal_file,
 }
